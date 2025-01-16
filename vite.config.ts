8d32d50c5a5c9fa804a2f251a8ec79fe9c3f00ea
JIHOON LEE <jihoon7705@gmail.com>
2025-01-16 14:45:56 +0900

    feat : vite 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
+import { defineConfig } from "vite";
 import styleX from "vite-plugin-stylex";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [reactRouter(), styleX()],
+  plugins: [reactRouter(), styleX(), tsconfigPaths()],
 });
