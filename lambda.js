ce3dd63c56efc68925e8a7512c19d76969620769
rojiwon123 <dev.rojiwon@gmail.com>
2025-01-17 12:28:43 +0900

    error page 수정

--- lambda.js
+++ lambda.js
@@ -21,10 +21,10 @@ const err_body = `<!doctype html>
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>ERROR PAGE</title>
+    <title>ERROR</title>
   </head>
   <body>
-    Internal Server ERROR
+    Internal Server Error
   </body>
 </html>
 `;
