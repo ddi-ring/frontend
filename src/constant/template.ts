fe60f340c4e2d3b7a29e658776f390594c50eef0
y09n <euihyun.yang.9x@gmail.com>
2025-01-18 18:03:57 +0900

    feat: update templates (#18)

--- src/constant/template.ts
+++ src/constant/template.ts
@@ -9,6 +9,48 @@ export const templates: Record<string, Template> = {
     id: "1",
     title: "크리스마스",
     thumbnailImageUrl:
-      "https://resource.ddi-ring.com/templates/템플릿1_크리스마스_bg.png",
+      "https://resource.ddi-ring.com/templates/template1_bg.png",
+  },
+  2: {
+    id: "2",
+    title: "생일파티",
+    thumbnailImageUrl:
+      "https://resource.ddi-ring.com/templates/template2_bg.png",
+  },
+  3: {
+    id: "3",
+    title: "와인파티",
+    thumbnailImageUrl:
+      "https://resource.ddi-ring.com/templates/template3_bg.png",
+  },
+  4: {
+    id: "4",
+    title: "크리스마스 파티",
+    thumbnailImageUrl:
+      "https://resource.ddi-ring.com/templates/template4_bg.png",
+  },
+  5: {
+    id: "5",
+    title: "우주",
+    thumbnailImageUrl:
+      "https://resource.ddi-ring.com/templates/template5_bg.png",
+  },
+  6: {
+    id: "6",
+    title: "산타",
+    thumbnailImageUrl:
+      "https://resource.ddi-ring.com/templates/template6_bg.png",
+  },
+  7: {
+    id: "7",
+    title: "트리",
+    thumbnailImageUrl:
+      "https://resource.ddi-ring.com/templates/template7_bg.png",
+  },
+  8: {
+    id: "8",
+    title: "할로윈",
+    thumbnailImageUrl:
+      "https://resource.ddi-ring.com/templates/template8_bg.png",
   },
 };
