f611a312996916fcdd02214ad3018e241c526117
JIHOON LEE <jihoon7705@gmail.com>
2025-01-18 11:36:55 +0900

    feat : 플로팅 버튼 생성

--- src/routes/create.template.$templateId.content._index.tsx
+++ src/routes/create.template.$templateId.content._index.tsx
@@ -1,5 +1,6 @@
 import { EventForm } from "@/components/EventForm.tsx";
 import * as stylex from "@stylexjs/stylex";
+import { useState } from "react";
 import { useNavigate } from "react-router";
 import type { Route } from "./+types/create.template.$templateId.content._index";
 
@@ -16,29 +17,72 @@ export default function Page({
   loaderData: { template },
 }: Route.ComponentProps) {
   const navigate = useNavigate();
+  const [isSelected, setIsSelected] = useState(false);
 
   return (
-    <div {...stylex.props(styles.container)}>
-      <header {...stylex.props(styles.header)}>
-        <div onClick={() => navigate(-1)} {...stylex.props(styles.backButton)}>
-          <img
-            src="/arrow-back.svg"
-            alt="뒤로 가기"
-            {...stylex.props(styles.backIcon)}
-          />
-        </div>
-        <h1 {...stylex.props(styles.headerTitle)}>내용 입력</h1>
-      </header>
+    <>
+      {isSelected ? (
+        <div {...stylex.props(styles.container)}>
+          <header {...stylex.props(styles.header)}>
+            <div
+              onClick={() => navigate(-1)}
+              {...stylex.props(styles.backButton)}
+            >
+              <img
+                src="/arrow-back.svg"
+                alt="뒤로 가기"
+                {...stylex.props(styles.backIcon)}
+              />
+            </div>
+            <h1 {...stylex.props(styles.headerTitle)}>내용 입력</h1>
+          </header>
 
-      <main {...stylex.props(styles.main)}>
-        <EventForm templateId={template.id} />
-      </main>
-    </div>
+          <main {...stylex.props(styles.main)}>
+            <EventForm templateId={template.id} />
+          </main>
+        </div>
+      ) : (
+        <div
+          {...stylex.props(
+            styles.container,
+            !isSelected && styles.selectedContainer,
+          )}
+        >
+          선택한 템플릿 이미지가 보일 예정
+          <button
+            type="button"
+            onClick={() => setIsSelected(true)}
+            {...stylex.props(styles.selectButton)}
+          >
+            이걸로 결정!
+          </button>
+        </div>
+      )}
+    </>
   );
 }
 
+const floating = stylex.keyframes({
+  "0%": {
+    transform: "translate(0, 0px)",
+  },
+  "50%": {
+    transform: "translate(0, 10px)",
+  },
+  "100%": {
+    transform: "translate(0, 0px)",
+  },
+});
+
 const styles = stylex.create({
   container: {
+    minHeight: "100vh",
+    backgroundColor: "#fff",
+  },
+  selectedContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
     minHeight: "100vh",
     backgroundColor: "#fff",
   },
@@ -79,5 +123,25 @@ const styles = stylex.create({
   main: {
     marginTop: 52,
     padding: "24px 16px",
+  },
+  selectButton: {
+    position: "fixed",
+    display: "flex",
+    bottom: "100px",
+    backgroundColor: "#FF731D",
+    border: "none",
+    borderRadius: 8,
+    color: "#fff",
+    justifyContent: "center",
+    cursor: "pointer",
+    fontSize: 16,
+    fontWeight: "bold",
+    padding: "16px",
+    width: "200px",
+    textAlign: "center",
+    animationName: floating,
+    animationDuration: "1.5s",
+    animationTimingFunction: "ease-in-out",
+    animationIterationCount: "infinite",
   },
 });
